49f109efc730d8681dbb9a66c3a9b1c894a52ccf
brunogomespascotto <panoramic56@proton.me>
2024-07-19 09:26:01 -0600

    Adds Javascript file and several stylistic changes

---
+++ script.js
@@ -0,0 +1,26 @@
+let intro = document.querySelector('.intro-div');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.intro-logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{ // All contents within this block will trigger whenever the page is loaded
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) =>{
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(() =>{
+            logoSpan.forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            }) 
+        }, 2000);
+
+        setTimeout(() =>{
+            intro.style.top = '-100vh';
+        }, 2300);
+    })
+})
